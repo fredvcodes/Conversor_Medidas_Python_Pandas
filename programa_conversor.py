f19f8bb21e4170f0eaa0e30fd88ec6ec0666be16
fredvcodes <fredv.codes@gmail.com>
2026-04-17 20:39:50 -0500

    Realizamos un programa para convertir medidas

---
+++ programa_conversor.py
@@ -0,0 +1,17 @@
+import pandas as pd
+
+
+def cm_a_pulgadas(cm):
+    return cm / 2.54
+
+
+# Leer el archivo Excel con las medidas en centímetros
+df = pd.read_excel("muebles_medidas.xlsx")
+
+# Añadir una columna al DataFrame que sea de pulgadas y se rellene con el calculo de cm / 2.54
+df["Pulgadas:"] = df["Centimeros"].apply(cm_a_pulgadas)
+
+
+df.to_excel("muebles_medidas_convertidas.xlsx", index=False)
+
+print("conversion completada y guardada en un nuevo archivo llamado 'muebles_medidas_convertidas.xlsx'")
